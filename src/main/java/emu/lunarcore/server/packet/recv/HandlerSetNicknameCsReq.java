c4d5277b36264966e5e031ae03c95ceb1f0873e6
a267764 <104370940+a267764@users.noreply.github.com>
2024-03-05 20:23:48 +0800

    Add files via upload

--- src/main/java/emu/lunarcore/server/packet/recv/HandlerSetNicknameCsReq.java
+++ src/main/java/emu/lunarcore/server/packet/recv/HandlerSetNicknameCsReq.java
@@ -5,6 +5,8 @@ import emu.lunarcore.server.game.GameSession;
 import emu.lunarcore.server.packet.CmdId;
 import emu.lunarcore.server.packet.Opcodes;
 import emu.lunarcore.server.packet.PacketHandler;
+import emu.lunarcore.server.packet.send.PacketSetNicknameScRsp;
+import emu.lunarcore.server.packet.send.PacketSetSignatureScRsp;
 
 @Opcodes(CmdId.SetNicknameCsReq)
 public class HandlerSetNicknameCsReq extends PacketHandler {
@@ -12,12 +14,13 @@ public class HandlerSetNicknameCsReq extends PacketHandler {
     @Override
     public void handle(GameSession session, byte[] data) throws Exception {
         var req = SetNicknameCsReq.parseFrom(data);
-        
+
         if (req.getNickname() != null) {
             session.getPlayer().setNickname(req.getNickname());
         }
-        
+
         session.send(CmdId.SetNicknameScRsp);
+        // session.send(new PacketSetNicknameScRsp(session.getPlayer()));
     }
 
 }
